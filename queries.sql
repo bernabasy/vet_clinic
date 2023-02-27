5c2512f2c23397a4206996b70bbd49d97c5e7819
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-27 23:40:04 +0300

    Write queries for  selecting values from table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name from animalS WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name from animals WHERE neutered = true and escape_attempts >3;
+SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name,escape_attempts from animals WHERE weight_kg >10.5;
+SELECT * from animals WHERE neutered = true;
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
