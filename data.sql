6b384fd57b88a9408b9906730c51cf147a7401ef
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-27 23:38:19 +0300

    The data  values pair inserted to animals table .

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts)
+values
+('Agumon', 'Feb 3, 2020', 10.23,'true', 0 ),
+('Gabumon', 'Nov 15, 2018', 8, 'true', 2),
+('Pikachu', 'Jan 7, 2021', 15.04, 'false', 1),
+('Devimon', 'May 12, 2017', 11, 'true', 5);
